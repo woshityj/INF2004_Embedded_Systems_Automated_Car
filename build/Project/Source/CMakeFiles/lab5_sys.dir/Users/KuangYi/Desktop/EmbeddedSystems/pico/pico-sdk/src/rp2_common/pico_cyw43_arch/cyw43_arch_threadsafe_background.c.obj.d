2c141903d8d3ea36e6c0b02c916cf808e628b7fe
NotAnima <iamnotabot969@gmail.com>
2023-10-09 03:32:07 +0800

    Thin format dependencies

---
+++ build/Project/Source/CMakeFiles/lab5_sys.dir/Users/KuangYi/Desktop/EmbeddedSystems/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_threadsafe_background.c.obj.d
@@ -0,0 +1,2 @@
+Project/Source/CMakeFiles/lab5_sys.dir/Users/KuangYi/Desktop/EmbeddedSystems/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_threadsafe_background.c.obj: \
+ /Users/KuangYi/Desktop/EmbeddedSystems/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_threadsafe_background.c
