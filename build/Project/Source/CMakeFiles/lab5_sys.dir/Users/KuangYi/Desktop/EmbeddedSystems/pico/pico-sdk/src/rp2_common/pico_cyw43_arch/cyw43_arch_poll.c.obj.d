90d0e9fabc81f4f8223f2dd44641ef5a3bea1cd6
NotAnima <iamnotabot969@gmail.com>
2023-10-18 20:05:45 +0800

    Added build to .gitignore

--- build/Project/Source/CMakeFiles/lab5_sys.dir/Users/KuangYi/Desktop/EmbeddedSystems/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj.d
+++
@@ -1,2 +0,0 @@
-Project/Source/CMakeFiles/lab5_sys.dir/Users/KuangYi/Desktop/EmbeddedSystems/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj: \
- /Users/KuangYi/Desktop/EmbeddedSystems/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
--- build/Project/tls_client/CMakeFiles/picow_tls_client_background.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for picow_tls_client_background.
-# This may be replaced when dependencies are built.
