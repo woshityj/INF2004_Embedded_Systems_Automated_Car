373dc1ab005fe86b6c8139e00ef64c7d1ee6ad88
EmpyreaLiv <2200795@sit.singaporetech.edu.sg>
2023-10-22 04:50:21 +0800

    Can now print barcode out in binary

--- src/infrared/infrared.c
+++ src/infrared/infrared.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <float.h>
 
 #include "pico/stdlib.h"
 
@@ -11,12 +12,16 @@
 
 void IR_scan();
 void IR_init();
+void formChar(float first, float second, float third);
+void findTopThree(float arr[], float *first, float *second, float *third);
+
+static int timings[MAX_TIMINGS];
+static float timing_differences[MAX_TIMINGDIFFERENCES];
+static int char_array[MAX_TIMINGDIFFERENCES];
 
 void IR_scan()
 {
     static bool flag = true;
-    static int timings[MAX_TIMINGS];
-    static float timing_differences[MAX_TIMINGDIFFERENCES];
     static int currentIndex = 0;
 
     if (gpio_get(IR_PIN) == 1)
@@ -51,10 +56,57 @@ void IR_scan()
             timing_differences[i] = second_timing_s - first_timing_s;
             printf("%f\n", timing_differences[i]); 
         }
+        float first, second, third;
+        findTopThree(timing_differences, &first, &second, &third);
+        formChar(first,second,third);
+        for (int i = 0; i < MAX_TIMINGDIFFERENCES; i++)
+        {
+            printf("%d", char_array[i]);
+        }
+        
+        printf("\n--- End of a set ---\n\n");  // This line is new
+        
         currentIndex = 0;
     }
     
     
+}
+
+void formChar(float first, float second, float third)
+{
+    for (int i = 0; i < MAX_TIMINGDIFFERENCES; i++) {
+        if (timing_differences[i] == first || timing_differences[i] == second || timing_differences[i] == third)
+        {
+            char_array[i] = 1;
+        } else
+        {
+            char_array[i] = 0;
+        }
+    }
+}
+
+void findTopThree(float arr[], float *first, float *second, float *third)
+{
+    *first = FLT_MIN;
+    *second = FLT_MIN;
+    *third = FLT_MIN;
+
+    for (int i = 0; i < MAX_TIMINGDIFFERENCES; i++)
+    {
+        if (arr[i] > *first)
+        {
+            *third = *second;
+            *second = *first;
+            *first = arr[i];
+        } else if (arr[i] > *second)
+        {
+            *third = *second;
+            *second = arr[i];
+        } else if (arr[i] > *third)
+        {
+            *third = arr[i];
+        }
+    }
 }
 
 void IR_init()
@@ -70,7 +122,7 @@ int main()
 
     while (true) {
         IR_scan();
-        sleep_ms(10);
+        sleep_us(10);
     }
 
     return 0;
