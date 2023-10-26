2a0e1fe482c0fcb043a81e091a49eabbe8209172
woshityj <yujietan84@gmail.com>
2023-10-27 00:21:30 +0800

    Updated Socket Client for Mac and Linux

--- src/wifi/socket_client_mac.c
+++ src/wifi/socket_client_mac.c
@@ -11,7 +11,7 @@ int main(int argc, char const* argv[])
 {
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
-    char* hello = "exit";
+    char* hello = "fat doggie style";
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\nSocket creation error\n");
@@ -36,17 +36,13 @@ int main(int argc, char const* argv[])
     int total_size = 2048;
     char buffer[2048] = { 0 };
 
-    while (total_size > 0)
-    {
-        valread = read(client_fd, buffer, 2048 - 1);
-        printf("%s\n", buffer);
-        printf("read %d bytes from server\n", valread);
-        total_size -= valread;
-    }
-
+    send(client_fd, hello, strlen(hello), 0);
+    printf("%s message sent\n", hello);
     sleep(3);
-    send(client_fd, hello, 2048, 0);
-    printf("Hello message sent\n");
+
+    valread = read(client_fd, buffer, 2048 - 1);
+    printf("%s\n", buffer);
+    printf("read %d bytes from server\n", valread);
 
     close(client_fd);
     return 0;
