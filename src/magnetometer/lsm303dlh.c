301265ece30a409dc07267a2c314d15113086e1b
Dereknan <50665229+Dereknan@users.noreply.github.com>
2023-10-27 16:45:50 +0800

    get relative position
    Gets relative position based on pico startup
    
    PINs used
    GPIO PICO_DEFAULT_I2C_SDA_PIN GPIO4 -> SDA
    GPIO PICO_DEFAULT_I2C_SCL_PIN GPIO5 -> SCL
    3.3v OUT -> VCC
    Any GND -> GND

---
+++ src/magnetometer/lsm303dlh.c
@@ -0,0 +1,150 @@
+#include <stdint.h>
+#include <math.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "lsm303dlh.h"
+
+// function --------------------------------------------------------------
+void lsm303dlh_acc_setup() {
+   uint8_t buffer[2] = {CTRL_REG_4, 0x00};
+   
+   i2c_write_blocking(
+      i2c_default,
+      INTERFACE_A,
+      buffer,
+      2,
+      true
+   );
+
+   buffer[0] = CTRL_REG_1;
+   buffer[1] = 0x27;
+   
+   i2c_write_blocking(
+      i2c_default,
+      INTERFACE_A,
+      buffer,
+      2,
+      false
+   );
+
+}
+
+// function --------------------------------------------------------------
+void lsm303dlh_mag_setup() {
+   uint8_t buffer[2] = {MAG_CRA, 0x10}; // 15 Hz refreshrate
+   
+   i2c_write_blocking(
+      i2c_default,
+      INTERFACE_B,
+      buffer,
+      2,
+      true
+   );
+
+   buffer[0] = MAG_CRB;
+   buffer[1] = 0xE0; // Gain - range +-8.1 Gauss, Gain X/Y and Z [LSB/Gauss] 230, GainZ [LSB/Gauss] 205
+   
+   i2c_write_blocking(
+      i2c_default,
+      INTERFACE_B,
+      buffer,
+      2,
+      true
+   );
+
+   buffer[0] = MAG_MR;
+   buffer[1] = 0x00; // Continuous-conversion mode
+   
+   i2c_write_blocking(
+      i2c_default,
+      INTERFACE_B,
+      buffer,
+      2,
+      false
+   );
+
+}
+
+// function --------------------------------------------------------------
+void lsm303dlh_read_acc(accel_t *acc) {
+   uint8_t buffer[6];
+   int16_t accel[3];
+   uint8_t reg = ACC_REG;
+
+   i2c_write_blocking(
+      i2c_default,
+      INTERFACE_A,
+      &reg,
+      1,
+      true
+   );
+
+   i2c_read_blocking(
+      i2c_default,
+      INTERFACE_A,
+      buffer,
+      6,
+      false
+   );
+
+   // merge uint8_t to int16_t
+   for (int i = 0; i < 3; i++) {
+      accel[i] = ((buffer[i * 2] | buffer[(i * 2) + 1]  << 8));
+   }
+
+   acc->x = accel[0];
+   acc->y = accel[1];
+   acc->z = accel[2];
+}
+
+// function --------------------------------------------------------------
+void lsm303dlh_read_mag(mag_t *mag) {
+   uint8_t buffer[6];
+   int16_t magnet[3];
+   uint8_t reg = MAG_REG;
+
+   i2c_write_blocking(
+      i2c_default,
+      INTERFACE_B,
+      &reg,
+      1,
+      true
+   );
+
+   i2c_read_blocking(
+      i2c_default,
+      INTERFACE_B,
+      buffer,
+      6,
+      false
+   );
+
+   // merge uint8_t to int16_t
+   for (int i = 0; i < 3; i++) {
+      magnet[i] = ((buffer[i * 2] << 8 | buffer[(i * 2) + 1] ));
+   }
+
+   mag->x = magnet[0];
+   mag->y = magnet[1];
+   mag->z = magnet[2];
+}
+
+// function --------------------------------------------------------------
+int32_t get_angle(mag_t *mag) {
+   // Calculate angle in radians
+   double angle_rad = atan2((double)mag->y, (double)mag->x);
+
+   // Convert angle to degrees
+   int32_t angle_deg = (int32_t)(angle_rad * (180.0 / M_PI));
+
+   // Normalize the angle to 0-360
+   if (angle_deg < 0) {
+       angle_deg += 360;
+   }
+
+   return angle_deg;
+}
+
+
+/* end of lsm303dlh.c */
---
+++ src/magnetometer/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+#include "lsm303dlh.h"
+
+#define I2C_BAUD 400 // 400 or 100 (kHz)
+#define REFRESH_PERIOD 100 // ms
+
+// functional declaration
+void init_i2c_default();
+
+// main ------------------------------------------------------------------
+int main() {
+   // check for default GPIO setup
+   
+   #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+      #warning Default I2C pins were not defined
+      puts("Default I2C pins were not defined");
+   #else
+   
+   // Default i2c, output setup
+   init_i2c_default();
+   stdio_init_all();
+
+   // Setup device
+   lsm303dlh_acc_setup();
+   lsm303dlh_mag_setup();
+
+   accel_t acc;
+   mag_t mag;
+
+   // Get initial angle
+   lsm303dlh_read_mag(&mag);
+   int32_t start_angle = get_angle(&mag);
+
+while (true) {
+   lsm303dlh_read_acc(&acc);
+   lsm303dlh_read_mag(&mag);
+   int32_t angle = get_angle(&mag);
+   int32_t used_angle = angle - start_angle;
+
+   // Normalize the angle to 0-360
+   if (used_angle < 0) {
+       used_angle += 360;
+   }
+
+    // Determine orientation
+    char* orientation;
+   if (used_angle >= 315 || used_angle < 45) {
+        orientation = "North";
+    } else if (used_angle >= 45 && used_angle < 135) {
+        orientation = "East";
+    } else if (used_angle >= 135 && used_angle < 225) {
+        orientation = "South";
+    } else if (used_angle >= 225 && used_angle < 315) {
+        orientation = "West";
+    }
+
+   //  printf("Mag. X =%4d Y =%4d, Z =%4d, Orientation: %s\n",
+   //         mag.x, mag.y, mag.z, orientation);
+    printf("Relative Degree: %4d, True Degree: %4d, Orientation: %s\n",
+           used_angle,angle, orientation);
+    sleep_ms(REFRESH_PERIOD);
+}
+
+
+   #endif
+   return 0;
+}
+
+// function --------------------------------------------------------------
+void init_i2c_default() {
+   i2c_init(i2c_default, I2C_BAUD * 1000);
+   gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+   gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+   gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+   gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+}
+
+/* end of main.c */
