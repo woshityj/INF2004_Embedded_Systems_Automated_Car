e7a8c45035845b4c883cf928ba4910770da70fbd
Dereknan <50665229+Dereknan@users.noreply.github.com>
2023-10-30 15:25:44 +0800

    Additional minor comments

--- src/magnetometer/lsm303dlh.c
+++ src/magnetometer/lsm303dlh.c
@@ -5,10 +5,11 @@
 #include "hardware/i2c.h"
 #include "lsm303dlh.h"
 
-// function --------------------------------------------------------------
+// Function to set up the accelerometer
 void lsm303dlh_acc_setup() {
    uint8_t buffer[2] = {CTRL_REG_4, 0x00};
    
+   // Write configuration to the accelerometer
    i2c_write_blocking(
       i2c_default,
       INTERFACE_A,
@@ -27,13 +28,13 @@ void lsm303dlh_acc_setup() {
       2,
       false
    );
-
 }
 
-// function --------------------------------------------------------------
+// Function to set up the magnetometer
 void lsm303dlh_mag_setup() {
-   uint8_t buffer[2] = {MAG_CRA, 0x10}; // 15 Hz refreshrate
+   uint8_t buffer[2] = {MAG_CRA, 0x10}; // 15 Hz refresh rate
    
+   // Write configuration to the magnetometer
    i2c_write_blocking(
       i2c_default,
       INTERFACE_B,
@@ -63,15 +64,15 @@ void lsm303dlh_mag_setup() {
       2,
       false
    );
-
 }
 
-// function --------------------------------------------------------------
+// Function to read accelerometer data
 void lsm303dlh_read_acc(accel_t *acc) {
    uint8_t buffer[6];
    int16_t accel[3];
    uint8_t reg = ACC_REG;
 
+   // Write the accelerometer register address
    i2c_write_blocking(
       i2c_default,
       INTERFACE_A,
@@ -80,6 +81,7 @@ void lsm303dlh_read_acc(accel_t *acc) {
       true
    );
 
+   // Read accelerometer data
    i2c_read_blocking(
       i2c_default,
       INTERFACE_A,
@@ -88,7 +90,7 @@ void lsm303dlh_read_acc(accel_t *acc) {
       false
    );
 
-   // merge uint8_t to int16_t
+   // Merge uint8_t values into int16_t
    for (int i = 0; i < 3; i++) {
       accel[i] = ((buffer[i * 2] | buffer[(i * 2) + 1]  << 8));
    }
@@ -98,12 +100,13 @@ void lsm303dlh_read_acc(accel_t *acc) {
    acc->z = accel[2];
 }
 
-// function --------------------------------------------------------------
+// Function to read magnetometer data
 void lsm303dlh_read_mag(mag_t *mag) {
    uint8_t buffer[6];
    int16_t magnet[3];
    uint8_t reg = MAG_REG;
 
+   // Write the magnetometer register address
    i2c_write_blocking(
       i2c_default,
       INTERFACE_B,
@@ -112,6 +115,7 @@ void lsm303dlh_read_mag(mag_t *mag) {
       true
    );
 
+   // Read magnetometer data
    i2c_read_blocking(
       i2c_default,
       INTERFACE_B,
@@ -120,18 +124,18 @@ void lsm303dlh_read_mag(mag_t *mag) {
       false
    );
 
-   // merge uint8_t to int16_t
+   // Merge uint8_t values into int16_t
    for (int i = 0; i < 3; i++) {
       magnet[i] = ((buffer[i * 2] << 8 | buffer[(i * 2) + 1] ));
    }
 
-   // Save x,y,z values into magnet array
+   // Save x, y, z values into mag struct
    mag->x = magnet[0];
    mag->y = magnet[1];
    mag->z = magnet[2];
 }
 
-// function --------------------------------------------------------------
+// Function to calculate the angle from magnetometer data
 int32_t get_angle(mag_t *mag) {
    // Calculate angle in radians
    double angle_rad = atan2((double)mag->y, (double)mag->x);
@@ -139,13 +143,12 @@ int32_t get_angle(mag_t *mag) {
    // Convert angle to degrees
    int32_t angle_deg = (int32_t)(angle_rad * (180.0 / M_PI));
 
-   // Normalise the angle to 0-360
+   // Normalize the angle to 0-360
    if (angle_deg < 0) {
        angle_deg += 360;
    }
 
    return angle_deg;
 }
-
 
 /* end of lsm303dlh.c */
--- src/magnetometer/magnetometer.c
+++ src/magnetometer/magnetometer.c
@@ -7,73 +7,72 @@
 #include "hardware/i2c.h"
 #include "lsm303dlh.h"
 
-#define I2C_BAUD 400 // 400 or 100 (kHz)
-#define REFRESH_PERIOD 100 // ms
+#define I2C_BAUD 400 // I2C baud rate (400 kHz)
+#define REFRESH_PERIOD 100 // Refresh period in milliseconds
 
-// functional declaration
+// Function declaration
 void init_i2c_default();
 
 // main ------------------------------------------------------------------
 int main() {
-   // check for default GPIO setup
-   
+   // Check for default GPIO setup
    #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
       #warning Default I2C pins were not defined
       puts("Default I2C pins were not defined");
    #else
-   
-   // Default i2c, output setup
+   // Default I2C, output setup
    init_i2c_default();
    stdio_init_all();
 
    // Setup device
-   lsm303dlh_acc_setup();
+//    lsm303dlh_acc_setup();
    lsm303dlh_mag_setup();
 
-   accel_t acc;
+//    accel_t acc;
    mag_t mag;
 
    // Get initial angle
    lsm303dlh_read_mag(&mag);
    int32_t start_angle = get_angle(&mag);
 
-while (true) {
-   lsm303dlh_read_acc(&acc);
-   lsm303dlh_read_mag(&mag);
-   int32_t angle = get_angle(&mag);
-   int32_t used_angle = angle - start_angle;
+   while (true) {
+    //    lsm303dlh_read_acc(&acc);
+       lsm303dlh_read_mag(&mag);
+       int32_t angle = get_angle(&mag);
+       int32_t used_angle = angle - start_angle;
 
-   // Normalise the angle to 0-360
-   if (used_angle < 0) {
-       used_angle += 360;
+       // Normalize the angle to 0-360
+       if (used_angle < 0) {
+           used_angle += 360;
+       }
+
+       // Determine orientation
+       char* orientation;
+       if (used_angle >= 315 || used_angle < 45) {
+           orientation = "North";
+       } else if (used_angle >= 45 && used_angle < 135) {
+           orientation = "East";
+       } else if (used_angle >= 135 && used_angle < 225) {
+           orientation = "South";
+       } else if (used_angle >= 225 && used_angle < 315) {
+           orientation = "West";
+       }
+
+    // printf("Acc. X = %5d Y = %5d, Z = %5d",
+    //             acc.x,acc.y,acc.z);
+       printf("Mag. X =%4d Y =%4d, Z =%4d, Orientation: %s\n",
+              mag.x, mag.y, mag.z, orientation);
+       // printf("Relative Degree: %4d, True Degree: %4d, Orientation: %s\n",
+       //        used_angle, angle, orientation);
+       sleep_ms(REFRESH_PERIOD);
    }
-
-    // Determine orientation
-    char* orientation;
-   if (used_angle >= 315 || used_angle < 45) {
-        orientation = "North";
-    } else if (used_angle >= 45 && used_angle < 135) {
-        orientation = "East";
-    } else if (used_angle >= 135 && used_angle < 225) {
-        orientation = "South";
-    } else if (used_angle >= 225 && used_angle < 315) {
-        orientation = "West";
-    }
-
-    printf("Mag. X =%4d Y =%4d, Z =%4d, Orientation: %s\n",
-           mag.x, mag.y, mag.z, orientation);
-    // printf("Relative Degree: %4d, True Degree: %4d, Orientation: %s\n",
-    //        used_angle,angle, orientation);
-    sleep_ms(REFRESH_PERIOD);
-}
-
-
    #endif
    return 0;
 }
 
 // function --------------------------------------------------------------
 void init_i2c_default() {
+   // Initialize I2C with the default settings
    i2c_init(i2c_default, I2C_BAUD * 1000);
    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
