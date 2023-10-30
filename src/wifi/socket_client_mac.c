ba0736b2b80c1745b0e68bc84546b7fb0113181c
adrrennn <2200865@sit.singaporetech.edu.sg>
2023-10-30 22:10:43 +0800

    Added README for wifi and comment on codes

--- src/wifi/socket_client_mac.c
+++ src/wifi/socket_client_mac.c
@@ -11,13 +11,22 @@ int main(int argc, char const* argv[])
 {
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
-    char* hello = "fat doggie style";
+    char* hello = "Hello Server";
+
+    // ----------------------
+    // Create a SOCKET for connecting to server
+    //
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\nSocket creation error\n");
         return -1;
     }
 
+
+    // ----------------------
+    // The sockaddr_in structure to specify address family,
+    // IP address, and port of the server to be connected to.
+    //
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
@@ -27,6 +36,9 @@ int main(int argc, char const* argv[])
         return -1;
     }
 
+    // ----------------------
+    // Connect to server
+    // 
     if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0)
     {
         printf("\nConnection Failed\n");
@@ -36,14 +48,23 @@ int main(int argc, char const* argv[])
     int total_size = 2048;
     char buffer[2048] = { 0 };
 
+    // ---------------------------
+    // Send message to server
+    // 
     send(client_fd, hello, strlen(hello), 0);
     printf("%s message sent\n", hello);
     sleep(3);
 
+    // ---------------------------
+    // Receive reply from server
+    // 
     valread = read(client_fd, buffer, 2048 - 1);
     printf("%s\n", buffer);
     printf("read %d bytes from server\n", valread);
 
+    // ----------------------
+    // Closing socket
+    // 
     close(client_fd);
     return 0;
 }
