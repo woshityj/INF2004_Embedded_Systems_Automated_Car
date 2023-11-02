975828ca4bf3da16eebf261f666961b7a3640533
NotAnima <iamnotabot969@gmail.com>
2023-11-02 17:20:36 +0800

    Added into .h file

--- src/mapping/mapper.h
+++ src/mapping/mapper.h
@@ -43,5 +43,6 @@ void generateNeighborCellsForNorth(Cell *currentCell);
 void generateNeighborCellsForSouth(Cell *currentCell);
 void generateNeighborCellsForEast(Cell *currentCell);
 void generateNeighborCellsForWest(Cell *currentCell);
+Cell** getMap(Set *set);
 
 #endif
