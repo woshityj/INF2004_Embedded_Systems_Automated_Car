db5d10d451523dbb0cddea95a474b86212b85736
woshityj <yujietan84@gmail.com>
2023-11-05 22:52:24 +0800

    Added Backwards

--- src/fusion/motor_and_encoder.c
+++ src/fusion/motor_and_encoder.c
@@ -32,6 +32,8 @@ void move_forward_with_distance(int cm)
     int speed = 80;
 
     struct repeating_timer timer;
+    
+    distance_completed = false;
 
     // Reset total distance travelled for each wheel
     left_interrupts = 0;
@@ -54,6 +56,38 @@ void move_forward_with_distance(int cm)
     stop()
 
     print("[Motor & Encoder] Moved Forward %dcm", cm);
+
+    return;
+}
+
+void move_backward_with_distance(int cm)
+{
+    int no_of_interrupts = cm_to_interrupts(cm);
+
+    int speed = 80;
+
+    struct repeating_timer timer;
+
+    distance_completed = false;
+
+    left_interrupts = 0;
+    right_interrupts = 0;
+
+    move_backward();
+
+    set_speed(speed, MOTOR_LEFT);
+    set_speed(speed, MOTOR_RIGHT);
+
+    add_repeating_timer_ms(-10, repeating_timer_callback_isr, &no_of_interrupts, &timer);
+
+    while (!distance_completed)
+    {
+        sleep_ms(10);
+    }
+
+    stop();
+
+    print("[Motor & Encoder] Moved Backwards %dcm", cm);
 
     return;
 }
