115cb3f0841a8d4cfec805d844a0fa537b40ccef
NotAnima <iamnotabot969@gmail.com>
2023-11-18 20:38:24 +0800

    Added getObstacle() function which returns true or false

--- src/ultrasonic/ultrasonic.h
+++ src/ultrasonic/ultrasonic.h
@@ -4,6 +4,8 @@
 #define TRIGGER_PIN 14
 #define ECHO_PIN 15
 #define SAMPLING_RATE 30
+#define ZERO_THRESHOLD 20 // 66% of sampling_rate
+#define DISTANCE_THRESHOLD 300 // in MM, 300mm == 30cm
 #define TIMEOUT 26100
 
 #include <stdio.h>
@@ -17,6 +19,7 @@ void echo_interrupt(uint gpio, uint32_t events);
 bool pullTrigger(struct repeating_timer *t);
 unsigned short getMM();
 int64_t alarm_pulldown_callback(alarm_id_t id, void *user_data);
+bool getObstacle();
 
 absolute_time_t startTime;
 absolute_time_t endTime;
